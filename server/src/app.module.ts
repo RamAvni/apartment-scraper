78f3f3f8dfda0e34226136a722a9fb61dc4a5eb0
RamAvni <ramavni@hilma.tech>
2025-08-09 16:35:38 +0300

    feat(server): nestjs/config - .env files setup

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { CrawlerModule } from "./crawler/crawler.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [CrawlerModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [
+        `.env.${process.env.NODE_ENV}.local`,
+        `.env.${process.env.NODE_ENV}`,
+      ],
+    }),
+    CrawlerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

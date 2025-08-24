c372163b8ddc806bdb0f275ff4da51aec3848df0
RamAvni <ramavni@hilma.tech>
2025-08-24 08:19:22 +0300

    feat(ollama): added ollama-server

--- server/src/crawler/crawler.controller.ts
+++ server/src/crawler/crawler.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Param, Post } from "@nestjs/common";
 import { CrawlerService } from "./crawler.service";
 import { scrapeDto } from "./dtos/scrape.dto";
+import ollama from "ollama";
 
 @Controller("crawler")
 export class CrawlerController {
@@ -9,5 +10,14 @@ export class CrawlerController {
   @Post()
   async scrape(@Body() body: scrapeDto) {
     return await this.crawlerService.scrape(body.urls);
+  }
+
+  @Post(":message")
+  async ollama(@Param("message") message: string) {
+    console.log(message);
+    return await ollama.chat({
+      model: "llama3.1",
+      messages: [{ role: "user", content: message }],
+    });
   }
 }
